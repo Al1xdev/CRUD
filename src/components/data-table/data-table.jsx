a21ce4e81186fc68526fdda4c285abd2f41f2a95
Alex <alex.horn.98@mail.ru>
2021-04-25 18:44:46 +0600

    Fix adaptivity

--- src/components/data-table/data-table.jsx
+++ src/components/data-table/data-table.jsx
@@ -11,7 +11,7 @@ import "./data-table.css";
 const DataTable = ({ data }) => {
   const dispatch = useDispatch();
 
-  const handlerDeleteUser = id => dispatch(deleteUser(id));
+  const handlerDeleteUser = (id) => dispatch(deleteUser(id));
 
   const handlerEditUser = (id, visible) => dispatch(editUser(id, visible));
 
@@ -19,42 +19,47 @@ const DataTable = ({ data }) => {
 
   return (
     <div className="main">
-      <div className="add-top" onClick={openModalPage}><img src={AddIcon} className="add" alt="Add"/></div>
-      <div>
+      <div onClick={openModalPage}>
+        <img src={AddIcon} className="add" alt="Add" />
+      </div>
+      <div className="main-container">
         <table>
-        <thead>
-          <tr className="table-head under-line">
-            <th>Name</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => {
-            return (
-              <tr key={item.id}>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>
-                  <button
-                    className="btn btn-edit"
-                    onClick={() => {openModalPage(); handlerEditUser(item.id, true)}}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    className="btn btn-del"
-                    onClick={() => handlerDeleteUser(item.id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Modal />
+          <thead>
+            <tr className="table-head under-line">
+              <th>Name</th>
+              <th>Email</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>
+                    <button
+                      className="btn btn-edit"
+                      onClick={() => {
+                        openModalPage();
+                        handlerEditUser(item.id, true);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="btn btn-del"
+                      onClick={() => handlerDeleteUser(item.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Modal />
       </div>
     </div>
   );
--- src/components/loading/laoding.jsx
+++ src/components/loading/laoding.jsx
@@ -4,19 +4,21 @@ import "./loader.css";
 
 const LaodingIndicator = () => {
   return (
-    <div className="lds-spinner">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="wrapper">
+      <div className="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
